7bcaaec5a5500f0202d87bd6ac2ce9de4d3c271b
eylulzehrakablan <eylulzehrakablan@gmail.com>
2026-07-25 00:50:03 +0300

    build MazeGenerator including a basic grid structure

---
+++ mazegen.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class MazeGenerator:
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
+        self.grid: List[List[int]] = []
+
+        for _ in range(height):
+            row: List[int] = []
+            for _ in range(height):
+                row.append(15)
+            self.grid.append(row)
+
+    def print_grid(self) -> None:
+        for row in self.grid:
+            for cell in row:
+                print(cell, end=" ")
+            print()

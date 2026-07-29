a5b9a0f2b4a42cd8faa96a6484652bfa30a7c40b
eylulzehrakablan <eylulzehrakablan@gmail.com>
2026-07-29 14:49:57 +0300

    embed 42 pattern to the grid and blocked_cells

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,4 +2,7 @@ from mazegen import MazeGenerator
 
 if __name__ == "__main__":
     maze = MazeGenerator(10, 10)
-    maze.print_grid()
+    maze.print_hex_maze()
+    maze.embed_42_pattern()
+    print()
+    maze.print_hex_maze()
--- mazegen.py
+++ mazegen.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set, Tuple
 
 
 class MazeGenerator:
@@ -7,6 +7,7 @@ class MazeGenerator:
         self.height = height
 
         self.grid: List[List[int]] = []
+        self.blocked_cells: Set[Tuple[int, int]] = set()
 
         for _ in range(height):
             row: List[int] = []
@@ -14,8 +15,35 @@ class MazeGenerator:
                 row.append(15)
             self.grid.append(row)
 
-    def print_hex_grid(self) -> None:
-        for row in self.grid:
-            for cell in row:
-                print(cell, end=" ")
-            print()
+    def embed_42_pattern(self) -> None:
+        start_x = round((self.width - 7) / 2)
+        start_y = round((self.height - 5) / 2)
+
+        pattern = [
+            (0, 0), (0, 1), (0, 2),
+            (1, 2), (2, 0), (2, 1),
+            (2, 2), (2, 3), (2, 4),
+            (4, 0), (5, 0), (6, 0),
+            (6, 1), (4, 2), (5, 2),
+            (6, 2), (4, 3), (4, 4),
+            (5, 4), (6, 4)
+        ]
+
+        for dx, dy in pattern:
+            self.blocked_cells.add((start_x + dx, start_y + dy))
+
+
+    def print_hex_maze(self) -> None:
+        for y in range(self.height):
+            line = ""
+            for x in range(self.width):
+                cell = self.grid[y][x]
+
+                if (x, y) in self.blocked_cells:
+                    line += "🟥"
+                elif cell == 15:
+                    line += "⬜️"
+                else:
+                    line += " "
+            print(line)
+
